f2bd505d8dd2e4933d3f125efa1685fe22fcd49f
evil <jaureswrld@gmail.com>
2026-03-11 09:06:33 +0000

    instalation du font google

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,13 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 import "./globals.css";
+import { Inter } from 'next/font/google';
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Spécifiez les poids nécessaires
+  display: 'swap',
+});
 
 export const metadata = {
   title: "Les petits plats",
@@ -12,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
+    <html lang="fr" className={inter.className}>
       <body>
         {children}
         <Footer />
